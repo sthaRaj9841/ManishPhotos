4e09fa2bca6775421f2031282afcc4f7d1fe011b
sthaRaj9841 <thaneshworshrestha499@gmail.com>
2025-05-18 10:50:10 +0545

    small changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Landing from "../components/Landing/Landing";
 import About from "../components/About/About";
-// import lifestyle from "../assets/lifestyle.jpg";
 import landscape from "../assets/landscape2.jpg";
+import lifestyle from "../assets/lifestyle.jpg";
 import animal from "../assets/animal2.jpg";
 import people from "../assets/people.jpg";
 import Card from "../components/Card/Card";
-
 const Home = () => {
   return (
     <div className="font-Pacifico">
       <Landing />
       <About />
-      {/* <Card image={lifestyle} title="lifestyle" /> */}
+      <Card image={lifestyle} title="lifestyle" />
       <Card image={landscape} title="landscape" />
       <Card image={animal} title="animal" />
       <Card image={people} title="people" />
